ca8ddc753df2622bd85c6e722352d969b27ccc34
DASARI SAIBABU <107811202+Dasari001@users.noreply.github.com>
2023-05-02 10:08:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+var pro = function(){
+	return new Promise((resolve,reject)=>{
+		setTimeout(()=>{
+			document.getElementById("output").innerText="Hello, world!";
+
+		},1000)
+	})
+}
